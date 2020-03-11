616523a1ab3fc6a42786dbfd2bff349798f81f99
Toasty <43488362+MisterToasty@users.noreply.github.com>
2020-03-10 17:10:09 -0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace TPAD
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.downloadButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.nextButton = new System.Windows.Forms.Button();
+            this.choices = new System.Windows.Forms.ComboBox();
+            this.siteChoice = new System.Windows.Forms.ComboBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.dniButton = new System.Windows.Forms.Button();
+            this.smdButton = new System.Windows.Forms.Button();
+            this.prevImgButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // downloadButton
+            // 
+            this.downloadButton.FlatAppearance.BorderSize = 0;
+            this.downloadButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.downloadButton.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.downloadButton.Location = new System.Drawing.Point(292, 415);
+            this.downloadButton.Name = "downloadButton";
+            this.downloadButton.Size = new System.Drawing.Size(75, 23);
+            this.downloadButton.TabIndex = 0;
+            this.downloadButton.Text = "Download Image";
+            this.downloadButton.UseVisualStyleBackColor = true;
+            this.downloadButton.Click += new System.EventHandler(this.downloadButton_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(650, 348);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // nextButton
+            // 
+            this.nextButton.FlatAppearance.BorderSize = 0;
+            this.nextButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.nextButton.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.nextButton.Location = new System.Drawing.Point(373, 415);
+            this.nextButton.Name = "nextButton";
+            this.nextButton.Size = new System.Drawing.Size(75, 23);
+            this.nextButton.TabIndex = 2;
+            this.nextButton.Text = "Next Image";
+            this.nextButton.UseVisualStyleBackColor = true;
+            this.nextButton.Click += new System.EventHandler(this.nextButton_Click);
+            // 
+            // choices
+            // 
+            this.choices.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(60)))));
+            this.choices.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.choices.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.choices.FormattingEnabled = true;
+            this.choices.Location = new System.Drawing.Point(667, 39);
+            this.choices.Name = "choices";
+            this.choices.Size = new System.Drawing.Size(121, 21);
+            this.choices.TabIndex = 3;
+            this.choices.Text = "avatar";
+            // 
+            // siteChoice
+            // 
+            this.siteChoice.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(60)))));
+            this.siteChoice.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.siteChoice.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.siteChoice.FormattingEnabled = true;
+            this.siteChoice.Location = new System.Drawing.Point(667, 12);
+            this.siteChoice.Name = "siteChoice";
+            this.siteChoice.Size = new System.Drawing.Size(121, 21);
+            this.siteChoice.TabIndex = 4;
+            this.siteChoice.Text = "nekos.life";
+            this.siteChoice.SelectedIndexChanged += new System.EventHandler(this.siteChoice_SelectedIndexChanged);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // dniButton
+            // 
+            this.dniButton.FlatAppearance.BorderSize = 0;
+            this.dniButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.dniButton.ForeColor = System.Drawing.Color.White;
+            this.dniButton.Location = new System.Drawing.Point(697, 415);
+            this.dniButton.Name = "dniButton";
+            this.dniButton.Size = new System.Drawing.Size(91, 23);
+            this.dniButton.TabIndex = 5;
+            this.dniButton.Text = "Start Mass";
+            this.dniButton.UseVisualStyleBackColor = true;
+            this.dniButton.Click += new System.EventHandler(this.dniButton_Click);
+            // 
+            // smdButton
+            // 
+            this.smdButton.FlatAppearance.BorderSize = 0;
+            this.smdButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.smdButton.ForeColor = System.Drawing.Color.White;
+            this.smdButton.Location = new System.Drawing.Point(12, 415);
+            this.smdButton.Name = "smdButton";
+            this.smdButton.Size = new System.Drawing.Size(91, 23);
+            this.smdButton.TabIndex = 6;
+            this.smdButton.Text = "Stop Mass";
+            this.smdButton.UseVisualStyleBackColor = true;
+            this.smdButton.Click += new System.EventHandler(this.smdButton_Click);
+            // 
+            // prevImgButton
+            // 
+            this.prevImgButton.FlatAppearance.BorderSize = 0;
+            this.prevImgButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.prevImgButton.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.prevImgButton.Location = new System.Drawing.Point(211, 415);
+            this.prevImgButton.Name = "prevImgButton";
+            this.prevImgButton.Size = new System.Drawing.Size(75, 23);
+            this.prevImgButton.TabIndex = 7;
+            this.prevImgButton.Text = "Prev Image";
+            this.prevImgButton.UseVisualStyleBackColor = true;
+            this.prevImgButton.Click += new System.EventHandler(this.prevImgButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(50)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.prevImgButton);
+            this.Controls.Add(this.smdButton);
+            this.Controls.Add(this.dniButton);
+            this.Controls.Add(this.siteChoice);
+            this.Controls.Add(this.choices);
+            this.Controls.Add(this.nextButton);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.downloadButton);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "MainForm";
+            this.Text = "TPAD";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button downloadButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button nextButton;
+        private System.Windows.Forms.ComboBox choices;
+        private System.Windows.Forms.ComboBox siteChoice;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button dniButton;
+        private System.Windows.Forms.Button smdButton;
+        private System.Windows.Forms.Button prevImgButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,238 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Windows.Forms;
+
+namespace TPAD
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        public Image DownloadImageFromUrl(string imageUrl)
+        {
+            Image image = null;
+
+            try
+            {
+                HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create(imageUrl);
+                webRequest.AllowWriteStreamBuffering = true;
+                webRequest.Timeout = 30000;
+
+                WebResponse webResponse = webRequest.GetResponse();
+
+                Stream stream = webResponse.GetResponseStream();
+
+                image = Image.FromStream(stream);
+
+                webResponse.Close();
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+
+            return image;
+        }
+
+        public string[] sites = new string[] { "nekos.life", "nekobot.xyz", "neko-love.xyz", "oboobs.ru", "obutts.ru" };
+        public string[] nekoslifechoices = new string[] { "femdom", "tickle", "classic", "ngif", "erofeet", "meow", "erok", "poke", "les", "v3", "hololewd", "nekoapi_v3.1", "lewdk", "keta", "feetg", "nsfw_neko_gif", "eroyuri", "kiss", "8ball", "kuni", "tits", "pussy_jpg", "cum_jpg", "pussy", "lewdkemo", "lizard", "slap", "lewd", "cum", "cuddle", "spank", "smallboobs", "goose", "Random_hentai_gif", "avatar", "fox_girl", "nsfw_avatar", "hug", "gecg", "boobs", "pat", "feet", "smug", "kemonomimi", "solog", "holo", "wallpaper", "bj", "woof", "yuri", "trap", "anal", "baka", "blowjob", "holoero", "feed", "neko", "gasm", "hentai", "futanari", "ero", "solo", "waifu", "pwankg", "eron", "erokemo" };
+        public string[] nekobotxyzchoices = new string[] { "hass", "hmidriff", "pgif", "4k", "hentai", "holo", "hneko", "neko", "hkitsune", "kemonomimi", "anal", "hanal", "kanna", "ass", "pussy", "thigh", "hthigh", "gah", "coffee", "food" };
+        public string[] nekolovechoices = new string[] { "nekolewd", "neko", "kitsune", "hug", "pat", "waifu", "cry", "kiss" };
+        public string[] oboobschoices = new string[] { "default" };
+        public string[] previmg = new string[] { };
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            siteChoice.Items.AddRange(sites);
+            choices.Items.AddRange(nekoslifechoices);
+            randomImage();
+        }
+
+        private string randomString()
+        {
+            var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            var stringChars = new char[8];
+            var random = new Random();
+
+            for (int i = 0; i < stringChars.Length; i++)
+            {
+                stringChars[i] = chars[random.Next(chars.Length)];
+            }
+
+            var finalString = new string(stringChars);
+            return finalString;
+        }
+
+        public string imgurl, fileName, ext, randomName;
+
+        //public string rootPath = @"E:\Toasty's Python Projects\TPAD\c#\TPAD\images";
+        public string rootPath;
+
+        public bool firstchoice = true;
+
+        private void siteChoice_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (firstchoice == true)
+            {
+                switch (siteChoice.Text)
+                {
+                    case "nekos.life":
+                        choices.Items.Clear();
+                        choices.Items.AddRange(nekoslifechoices);
+                        choices.Text = "femdom";
+                        break;
+
+                    case "nekobot.xyz":
+                        choices.Items.Clear();
+                        choices.Items.AddRange(nekobotxyzchoices);
+                        choices.Text = "hass";
+                        break;
+
+                    case "neko-love.xyz":
+                        choices.Items.Clear();
+                        choices.Items.AddRange(nekolovechoices);
+                        choices.Text = "nekolewd";
+                        break;
+
+                    case "oboobs.ru":
+                        choices.Items.Clear();
+                        choices.Items.AddRange(oboobschoices);
+                        choices.Text = "default";
+                        break;
+
+                    case "obutts.ru":
+                        choices.Items.Clear();
+                        choices.Items.AddRange(oboobschoices);
+                        choices.Text = "default";
+                        break;
+                }
+                Array.Clear(previmg, 0, previmg.Length);
+            }
+        }
+
+        public bool mdb = false;
+        public int imgnum = -1;
+
+        public void randomImage()
+        {
+            WebClient client = new WebClient();
+            string source = client.DownloadString("https://pastebin.com/raw/SKiMxc92");
+            dynamic data = JObject.Parse(source);
+            string rootPath = Directory.GetCurrentDirectory();
+            switch (siteChoice.Text)
+            {
+                case "nekos.life":
+                    //choices.Items.Clear();
+                    //choices.Items.AddRange(nekoslifechoices);
+                    //choices.Text = "femdom";
+                    source = client.DownloadString("https://nekos.life/api/v2/img/" + choices.Text);
+                    data = JObject.Parse(source);
+                    imgurl = data.url;
+                    break;
+
+                case "nekobot.xyz":
+                    source = client.DownloadString("https://nekobot.xyz/api/image?type=" + choices.Text);
+                    data = JObject.Parse(source);
+                    imgurl = data.message;
+                    break;
+
+                case "neko-love.xyz":
+                    source = client.DownloadString("https://neko-love.xyz/api/v1/" + choices.Text);
+                    data = JObject.Parse(source);
+                    imgurl = data.url;
+                    break;
+
+                case "oboobs.ru":
+                    source = client.DownloadString("http://api.oboobs.ru/boobs/0/1/random");
+                    JArray jsonArray2 = JArray.Parse(source);
+                    data = JObject.Parse(jsonArray2[0].ToString());
+                    imgurl = "http://media.oboobs.ru/" + data.preview;
+                    break;
+
+                case "obutts.ru":
+                    source = client.DownloadString("http://api.obutts.ru/butts/0/1/random");
+                    JArray jsonArray3 = JArray.Parse(source);
+                    data = JObject.Parse(jsonArray3[0].ToString());
+                    imgurl = "http://media.obutts.ru/" + data.preview;
+                    break;
+            }
+            ext = Path.GetExtension(imgurl);
+
+            //Image image = DownloadImageFromUrl(imgurl.Trim());
+            randomName = randomString() + ext;
+            rootPath = Directory.GetCurrentDirectory();
+            rootPath += "\\images\\";
+            rootPath += siteChoice.Text;
+            rootPath += "\\";
+            rootPath += choices.Text;
+            rootPath = rootPath.ToString();
+
+            Console.WriteLine(rootPath);
+            try
+            {
+                Directory.CreateDirectory(rootPath);
+            }
+            catch
+            {
+                return;
+            }
+            fileName = Path.Combine(rootPath, randomName);
+            if (mdb == false)
+            {
+                previmg = previmg.Append(imgurl).ToArray();
+                pictureBox1.ImageLocation = imgurl;
+                imgnum += 1;
+                Console.WriteLine(previmg[imgnum]);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            randomImage();
+            Image image = DownloadImageFromUrl(imgurl.Trim());
+            image.Save(fileName);
+        }
+
+        private void downloadButton_Click(object sender, EventArgs e)
+        {
+            Image image = DownloadImageFromUrl(imgurl.Trim());
+            image.Save(fileName);
+        }
+
+        private void dniButton_Click(object sender, EventArgs e)
+        {
+            mdb = true;
+            timer1.Tick += new EventHandler(timer1_Tick);
+            timer1.Interval = 500;
+            timer1.Start();
+        }
+
+        private void smdButton_Click(object sender, EventArgs e)
+        {
+            mdb = false;
+            timer1.Stop();
+        }
+
+        public void nextButton_Click(object sender, EventArgs e)
+        {
+            randomImage();
+        }
+
+        private void prevImgButton_Click(object sender, EventArgs e)
+        {
+            if (imgnum > 0)
+            {
+                imgnum -= 1;
+                Console.WriteLine(previmg[imgnum]);
+                pictureBox1.ImageLocation = previmg[imgnum];
+            }
+        }
+    }
+}
